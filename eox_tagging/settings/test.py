ec383b0b7eef7cdcbadf4c1d57352799ef8972a0
mariagrimaldi <maria.grimaldi@edunext.co>
2020-05-26 14:00:36 -0400

    added circleCI config using as template eox-tenant config. Also, changed the checksum for requirements/test.txt
    fixed install requirements
    
    fixed isort errors
    
    added future constraint
    
    added coverage to test.in

--- eox_tagging/settings/test.py
+++ eox_tagging/settings/test.py
@@ -4,9 +4,9 @@ Test Django settings for eox_tagging project.
 
 from __future__ import unicode_literals
 
+from .aws import *  # pylint: disable=wildcard-import
 from .common import *  # pylint: disable=wildcard-import
 from .production import *  # pylint: disable=wildcard-import
-from .aws import *  # pylint: disable=wildcard-import
 
 
 class SettingsClass(object):
