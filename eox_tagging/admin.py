98e84733bb6dbac016fd72624694444175bbed94
mariagrimaldi <maria.grimaldi@edunext.co>
2020-06-02 15:12:05 -0400

    - Fixed previous tests deleting unnecessary get. Also, added tests for validation with regex and more test cases for queries.
    - Added also list_display for admin (it made visualization using Admin site more friendly)

--- eox_tagging/admin.py
+++ eox_tagging/admin.py
@@ -10,7 +10,13 @@ from eox_tagging.models import Tag
 class TagAdmin(admin.ModelAdmin):
     """Tag admin
     """
-    pass
+
+    list_display = [
+        "tag_type",
+        "tag_value",
+        "tagged_object_name",
+        "belongs_to_object_name"
+    ]
 
 
 admin.site.register(Tag, TagAdmin)
