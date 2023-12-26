abea286e775e2ddb532849badc80457433ad883b
Luis Felipe Castano <felipe.castano@edunext.co>
2023-12-26 18:17:40 -0500

    fix: pylint R1705: Unnecessary "else" after "return"

--- eox_tagging/models.py
+++ eox_tagging/models.py
@@ -266,8 +266,7 @@ class Tag(models.Model):
         field_value = getattr(self, f"{attr}_type")
         if name:
             return field_value
-        else:
-            return getattr(self, attr)
+        return getattr(self, attr)
 
     def __get_field_choice(self, attr):
         """
