8ac271389429096067d3329ecac627a9e2c1ae76
Luis Felipe Castano <felipe.castano@edunext.co>
2024-06-04 23:14:14 -0500

    fix: eox-core requirement

--- eox_tagging/test/tutor/integration.sh
+++ eox_tagging/test/tutor/integration.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # This script installs the package in the edxapp environment, installs test requirements from Open edX and runs the tests using the Tutor settings
+echo "Install eox-core(requirement)"
+pip install eox-core
+
 echo "Install package"
 pip install -e ../eox-tagging
 
