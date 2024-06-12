4181cf017530d57bb83c8275d4d7d7393422f317
Luis Felipe Castaño <78836902+luisfelipec95@users.noreply.github.com>
2024-06-12 14:29:01 -0500

    feat: add tutor integration test (#104)
    * feat: add tutor integration test

--- eox_tagging/settings/test.py
+++ eox_tagging/settings/test.py
@@ -3,6 +3,11 @@ Test Django settings for eox_tagging project.
 """
 from __future__ import unicode_literals
 
+import codecs
+import os
+
+import yaml
+
 from .common import *  # pylint: disable=wildcard-import, unused-wildcard-import
 
 
@@ -45,6 +50,13 @@ def plugin_settings(settings):  # pylint: disable=function-redefined
     settings.EOX_TAGGING_GET_COURSE_OVERVIEW = "eox_tagging.edxapp_wrappers.backends.course_overview_i_v1_test"
     settings.EOX_TAGGING_GET_ENROLLMENT_OBJECT = "eox_tagging.edxapp_wrappers.backends.enrollment_l_v1_test"
     settings.TEST_SITE = 1
+
+    # setup the databases used in the tutor local environment
+    lms_cfg = os.environ.get('LMS_CFG')
+    if lms_cfg:
+        with codecs.open(lms_cfg, encoding='utf-8') as file:
+            env_tokens = yaml.safe_load(file)
+        settings.DATABASES = env_tokens['DATABASES']
 
 
 SETTINGS = SettingsClass()
