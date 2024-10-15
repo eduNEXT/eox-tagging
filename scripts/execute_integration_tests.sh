baa82e2780f3ceedeb77657375765edb128fb91a
magajh <maria.jaimes@edunext.co>
2024-10-14 22:32:27 -0400

    fix: integration test script

--- scripts/execute_integration_tests.sh
+++ scripts/execute_integration_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Install test-requirements"
-make install-dev-dependencies
+echo "Install requirements"
+make test_requirements
 
 echo "Run tests"
 pytest -rPf ./eox_tagging/test/integration --ignore=test_api_integration.py
